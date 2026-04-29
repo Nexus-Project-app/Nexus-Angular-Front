905d0bda0344537f0a0aa6c101dd3f14e93fa2aa
zzAdri <adrien.lafargue.adri@gmail.com>
2026-04-29 09:17:44 +0200

    feat(routing): add lazy-loaded route /politique-de-confidentialite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'politique-de-confidentialite',
+    loadChildren: () =>
+      import('./modules/privacy-policy/privacy-policy.routes').then(
+        (m) => m.PRIVACY_POLICY_ROUTES
+      ),
+  },
+];
